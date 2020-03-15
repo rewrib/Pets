40cf4394eaba591aadd4fc840909031cd382b393
rewrib <birwer.ali@gmx.de>
2020-03-15 15:18:15 +0100

    adding URI building blocks

--- app/src/main/java/com/example/android/pets/data/PetsContract.java
+++ app/src/main/java/com/example/android/pets/data/PetsContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 
@@ -19,6 +20,12 @@ public final class PetsContract {
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
+
+        // content provider variables
+        public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+        public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+        public static final String PATH_PETS = "pets";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
 
     }
